6da52c50c73939a12ea664f2ca30e10178786062
tomas <tomas@LAPTOP-4MMENUOK>
2020-02-27 22:52:58 +0100

    Enitity

---
+++ Arkanoid/Entity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Arkanoid
+{
+    public class Entity : INotifyPropertyChanged
+    {
+        #region Properties
+        private double x;
+        public double X
+        {
+            get { return this.x; }
+            set
+            {
+                if (this.x != value)
+                {
+                    this.x = value;
+                    this.NotifyPropertyChanged(nameof(X));
+                }
+            }
+        }
+
+        private double y;
+        public double Y
+        {
+            get { return this.y; }
+            set
+            {
+                if (this.y != value)
+                {
+                    this.y = value;
+                    this.NotifyPropertyChanged(nameof(Y));
+                }
+            }
+        }
+
+        public double Width { get; set; }
+        public double Height { get; set; }
+        #endregion
+
+        #region Change Handler
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        #endregion
+    }
+}
---
+++ Arkanoid/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Arkanoid
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Entity> AllEntities { get; set; } = new ObservableCollection<Entity>();
+
+        public ViewModel()
+        {
+            AllEntities.Add(new Entity { X = 10, Y = 20, Width = 200, Height = 500 });
+        }
+    }
+}
