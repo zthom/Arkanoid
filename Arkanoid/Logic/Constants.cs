b6d952f48500fde642b6202c5d29df37d0577e0d
tomas <tomas@LAPTOP-4MMENUOK>
2020-02-28 22:13:39 +0100

    faster

--- Arkanoid/Logic/Constants.cs
+++ Arkanoid/Logic/Constants.cs
@@ -23,7 +23,7 @@ namespace Arkanoid.Logic
         public static int BallSize => DefaultSize / 2;
         public static int BubbleSize => DefaultSize / 2;
 
-        public static double PadSpeed => 5;
+        public static double PadSpeed => 10;
         public static double BallSpeed => 5;
         public static double RocketSpeed => 5;
         public static double BubbleSpeed => 5;
